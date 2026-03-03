d6339f95aff99cded431cee43a501c8393e79a1c
gowsikraja <gowsik.andro@gmail.com>
2026-03-03 20:08:22 +0530

    IOS background permission fixes

--- ios/Classes/LocationService.swift
+++ ios/Classes/LocationService.swift
@@ -19,7 +19,7 @@ class LocationService: NSObject, CLLocationManagerDelegate {
         status = CLLocationManager.authorizationStatus()
       }
 
-      return status == .authorizedAlways
+      return status == .authorizedAlways || status == .authorizedWhenInUse
     }
 
 
